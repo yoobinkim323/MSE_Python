0dd21d45933a283130daca802cf16ef69bcc694d
yoobin kim <yoobin0323@jbnu.ac.kr>
2020-12-13 15:36:13 +0900

    added python file

---
+++ ex260.py
@@ -0,0 +1,6 @@
+class OMG : #클래스 정의
+    def print() : #클래스 안에 print 함수
+        print("Oh my god") #Oh my god을 출력하시오
+
+mystock = OMG() #객체 생성->mystock이라는 변수로 정의
+mystock.print() #OMG.print(mystock)로 자동으로 넘어가기 때문에 에러 발생     
